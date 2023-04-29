51ed4658164a13cc78c1d6518c4b6f177fc0d354
jose tano <josejonathan.tano@gmail.com>
2023-04-29 18:35:30 +0700

    a

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,7 +21,7 @@ export default function Home(){
     return (
         <div className="bg">
             <NavBar/>
-            <AnimeSlider/>
+            {/* <AnimeSlider/> */}
             <AnimeGrids/>
         </div>
     )
